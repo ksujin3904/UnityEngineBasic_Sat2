41866bb894914e954a7afb24010cbe9530dbcdae
ksujin3904 <ksujin2599@gmail.com>
2023-02-04 18:51:55 +0900

    Csharp-Variables,Methods
    [UnityEngineBasic]
    1. Visual Studio 2022 install
    2. Object Orientated Language
    3. Programming Language Level
    4. C# Basic
    - Variables
    - methods

---
+++ UnityEngineBasic/Csharp/Methods/Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+// 함수
+// 함수 선언 => C#에서는 추상화할때만 사용
+
+// 함수 정의
+// 반환타입 함수이름 (매개변수1타입 매개변수1이름, 매개변수2타입 매개변수2이름)
+// {
+//  return 반환값
+// }
+
+ int Sum(int op1,int op2)
+{
+    //return: 이 함수가 할당된 스택 영역의 메모리 제어권을 반환 + 함수의 연산 결과값을 반환
+    return op1 + op2;
+}
+
+// void: 정해진 타입이 없음
+void SayHello()
+{
+    Console.WriteLine("Hello");
+    return; // void 반환 하는 함수의 가장 마지막 라인의 return은 생략 가능.
+}
+
+int result = 0;
+// 함수 호출
+// 함수이름(인자1, 인자2...)
+result = Sum(1, 1);
+Console.WriteLine($"1 + 1 = { result}");
+
+// [함수 생성 예제]
+// 3가지 실수를 더하고 결과 콘솔창에 출력 후 연산결과를 반환하는 함수 작성
+
+double SumByThreeDouble(double op1, double op2, double op3)
+{
+    // 지역변수: { } 영억 안에서 선언되고 해당 여역을 벗어나면 메모리 해제되는 변수 <-> 전역변수
+    double result = op1 + op2 + op3;
+    Console.WriteLine($"double 형태 연산: {op1} + {op2} + {op3} = {result}");
+    return result;
+}
+
+SumByThreeDouble(1.1, 2.2, 3.3);
+
+float SumByThreeFloat(float op1, float op2, float op3)
+{
+    float result = op1 + op2 + op3;
+    Console.WriteLine($"float 형태 연산: {op1} + {op2} + {op3} = {result}");
+    return result;
+}
+
+SumByThreeFloat(1.1f, 2.2f, 3.3f);
---
+++ UnityEngineBasic/Csharp/Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿// 변수 (Variables)
+// 아직 정해지지 않은 값
+
+// 변수를 사용하겠다고 '선언' 하는 것의 의미
+//  => 특정 형태의 값이 들어갈 수 있는(쓸 수 있는) 메모리영역의 공간을 할당
+
+// 변수 선언 형태
+// 자료형 이름;
+
+// 초기화 형태
+// 자료형 이름 = 초기화값;
+
+int number = 25; // 부호가 있는 4byte 정수형
+short shortnumber; // 부호가 있는 2byte 정수형
+long longnumber; // 부호가 있는 8byte 정수형
+float floatnumber = 0.0f; // 4byte 실수형
+double doublenumber = 0.0; // 8byte 실수형
+
+char c = 'k'; // 2byte 문자형
+string s = "안녕"; // 2byte * 문자형 + 1byte (null) 문자열형
+bool isTrue = false; // 1byte 논리형
